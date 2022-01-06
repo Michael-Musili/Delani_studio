a536f3b6d90e82a5e57f42a1ff7f32362ef69aec
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2022-01-06 11:07:23 +0300

    added js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
- 
+$(document).ready(function(){
+  $(".icon-p").click(function(){
+    $(".toggle1").toggle();
+  })
+})
+$(document).ready(function(){
+  $(".icon-s").click(function(){
+    $(".toggle2").toggle();
+  })
+
+})
+
+$(document).ready(function(){
+  $(".icon-m").click(function(){
+    $(".toggle3").toggle();
+  })
+
+})
+$(document).ready(function(){
+  $(".hoover1").mouseover(function(){
+    $(".hide1").show();
+  });
+  $(".hoover1").mouseout(function(){
+    $(".hide1").hide();
+  });
+
+});
+
