1d10a641a5e9f889975ae79ad6d69b2efd5f0fcb
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2022-01-07 13:53:23 +0300

    added html.

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,8 @@ $(document).ready(function(){
   })
 
 })
+//hoover
+
 $(document).ready(function(){
   $(".hoover1").mouseover(function(){
     $(".hide1").show();
@@ -27,10 +29,81 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $("#btn1").click(function(){
-    alert($("#input1").val() + " "+ " We have received your message.Thank you for reaching out to us.");
-  
+  $(".hoover2").mouseover(function(){
+    $(".hide2").show();
   });
+  $(".hoover2").mouseout(function(){
+    $(".hide2").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".hoover3").mouseover(function(){
+    $(".hide3").show();
+  });
+  $(".hoover3").mouseout(function(){
+    $(".hide3").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".hoover4").mouseover(function(){
+    $(".hide4").show();
+  });
+  $(".hoover4").mouseout(function(){
+    $(".hide4").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".hoover5").mouseover(function(){
+    $(".hide5").show();
+  });
+  $(".hoover5").mouseout(function(){
+    $(".hide5").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".hoover6").mouseover(function(){
+    $(".hide6").show();
+  });
+  $(".hoover6").mouseout(function(){
+    $(".hide6").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".hoover7").mouseover(function(){
+    $(".hide7").show();
+  });
+  $(".hoover7").mouseout(function(){
+    $(".hide7").hide();
+  });
+
+});
+
+$(document).ready(function(){
+  $(".hoover8").mouseover(function(){
+    $(".hide8").show();
+  });
+  $(".hoover8").mouseout(function(){
+    $(".hide8").hide();
+  });
+
+});
+
+
+//hoover end
+
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    alert($("#input1").val() + " We have received your message.Thank you for reaching out to us.")
 
 });
 
@@ -50,6 +123,4 @@ function subscribeSomeoneToMailChimpList()
   var mcSubscribeRequest = UrlFetchApp.fetch("https://us4.api.mailchimp.com/2.0/lists/subscribe.json", options);
   var mcListObject = Utilities.jsonParse(mcSubscribeRequest.getContentText());
 }
-
-
-
+});
